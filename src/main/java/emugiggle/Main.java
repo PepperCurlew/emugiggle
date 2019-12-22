97c6111a54cdf7f822e0bf35f15999da01f3e087
emugiggle <emugiggle@nowhere.nul>
2019-12-22 14:45:32 -0500

    init

---
+++ src/main/java/emugiggle/Main.java
@@ -0,0 +1,12 @@
+package emugiggle;
+
+public class Main {
+
+public static void main(String[] args) {
+    System.out.println("[" +
+        org.apache.commons.lang3.StringUtils.trim(
+            "   giggle  giggle   . ")
+            + "]");
+}
+
+}
